3cd47a8ce4466a27d1b5d78f9a7affedb62bd678
mohit103 <72293411+mohit103@users.noreply.github.com>
2020-10-03 13:00:43 +0530

    Add prompts to SimpleInterest.java

--- Java/SimpleInterest.java
+++ Java/SimpleInterest.java
@@ -1,12 +1,16 @@
-import java.io*;
-public class SimpleInterest() {
+import java.io.*;
+public class SimpleInterest {
   public static void main(String args[]) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     double p, r, t;
+    System.out.print("Please input the principle amount: ");
     p = Double.parseDouble(br.readLine());
+    System.out.print("Please input the rate (%): ");
     r = Double.parseDouble(br.readLine());
+    System.out.print("Please input the time in years: ");
     t = Double.parseDouble(br.readLine());
     Double si = p*r*t/100;
-    System.out.println(si);
+    System.out.println("The simple interest is: "+si);
+    System.out.println("The final amount is: "+(p+si));
     }
    }
