cc2ce6d29de4af17b6c98db78e298e95f23c0742
riyam2002 <72618465+riyam2002@users.noreply.github.com>
2021-10-28 23:05:30 +0530

    Create ArrayDynamicMemSearch.c

---
+++ C/ArrayDynamicMemSearch.c
@@ -0,0 +1,44 @@
+// Write a C program to search an element in a 2D-Array using dynamic memory
+// allocation.
+#include<stdio.h>
+#include<process.h>
+#include<stdlib.h>
+void main()
+{
+    int i, n, m, j, num, c=0;
+    printf("Enter the dimensions: ");
+    scanf("%d%d", &n, &m);
+    int *p[n];
+    for(i=0;i<n;i++)
+    {
+        p[i]=(int*)calloc(m, sizeof(int));
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            printf("Number: ");
+            scanf("%d", &p[i][j]);
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+            printf("%d ", p[i][j]);
+        }
+    }
+    printf("\nEnter the number to be searched: ");
+    scanf("%d", &num);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<m;j++)
+        {
+              if(p[i][j]==num) 
+            {
+                c++;
+            }
+        }
+    }
+    printf("Element found %d time(s).\n", c);
+}
