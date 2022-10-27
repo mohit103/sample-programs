40057a506ba2f0738a34a79d42ce15aac84a4151
nissan7 <sdpnis7@gmail.com>
2022-10-27 17:42:35 +0530

    ammended code:

--- C/pallindrome.c
+++ C/pallindrome.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 void main()
 {  
-    int num, rev=0, k, onum;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    onum=num;
-    while(num!=0)
+    int long long num, rev=0, k, copy;
+    scanf("Enter a number %u", &num);
+    copy = num;
+    while(num!= 0)
     {
         k= num%10;
         rev= rev*10+k;
-        num/=10;
+        num /= 10;
     }
-    if(rev==onum) printf("It is a pallindrome\n");
-    else printf("It is not a pallindrome\n");
+
+    if(rev == copy) {
+	    printf("It is a pallindrome\n");
+    } else {
+	    printf("It is not a pallindrome\n");
+    }
 }
